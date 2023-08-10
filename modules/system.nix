3e97d08bd4c9ab59bdb6e9f22b92fdc8ecc5408f
SamueleFacenda <samuele.facenda@gmail.com>
2023-08-10 16:30:15 +0200

    Changed configuration p10k for a more emojier one
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- modules/system.nix
+++ modules/system.nix
@@ -104,4 +104,34 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
+  fonts = {
+   fonts = with pkgs; [
+      # icon fonts
+      material-design-icons
+
+      # normal fonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+
+      # nerdfonts
+      (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Monofur"]; })
+
+      # TODO: add monofur font for something
+    ];
+
+    # use fonts specified by user rather than default ones
+    enableDefaultFonts = false;
+
+    # user defined fonts
+    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
+    # B&W emojis that would sometimes show instead of some Color emojis
+    fontconfig.defaultFonts = {
+      serif = [ "Noto Serif" "Noto Color Emoji" ];
+      sansSerif = [ "Noto Sans" "Noto Color Emoji" ];
+      monospace = [ "JetBrainsMono Nerd Font" "Noto Color Emoji" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
+
 }
