fdcd6236995a1963446696c09fbe96c48f7a4e19
SamueleFacenda <samuele.facenda@gmail.com>
2023-08-26 16:02:40 +0200

    Fix nix develop command syntax in intellij desktop entry
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/programs/desktop-entries.nix
+++ home/programs/desktop-entries.nix
@@ -5,7 +5,7 @@
   	  genericName = "IntelliJ IDEA – the Leading Java and Kotlin IDE";
       comment = "The IDE that makes development a more productive and enjoyable experience";
       icon = "${pkgs.jetbrains.idea-ultimate}/share/pixmaps/idea-ultimate.svg";
-      exec = "nix-shell \"${self.outPath}#java\" --run idea-ultimate";
+      exec = "nix develop \"${self.outPath}#java\" --command idea-ultimate";
       terminal = false;
       categories = [ "Development" ];
     };
