eb369d1681d2a6b56557fc12139aa383eb329d96
SamueleFacenda <samuele.facenda@gmail.com>
2023-08-29 12:09:18 +0200

    Edit kitty conf
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -4,7 +4,7 @@
 		font.name = "Monofur Nerd Font Mono"; # "JetBrainsMono Nerd Font";
 		font.size = 15;
 		# theme = "Adwaita dark";
-
+        # theme = "GitHub Dark";
 		environment = {
 			# ZLE_RPROMPT_INDENT = "0";
 		};
@@ -13,6 +13,12 @@
 			clipboard_control = "write-clipboard write-primary read-clipboard read-primary";
 			disable-ligatures = "cursor";
 			# font_features = "MonofurNF "
+			background = "#292929";
+			background_opacity = "0.85";
+			background_blur = "30"; # not supported on wayland
+			dynamic_background_opacity = "yes";
+			
+			wayland_titlebar_color = "system";
 		};
 	};
 }
