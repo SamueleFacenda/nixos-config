17fa2e539cc9a1e7b015e507020f1495fbcff59f
SamueleFacenda <samuele.facenda@gmail.com>
2023-09-04 10:54:02 +0200

    Add hyprpaper config
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/hyprland/hyprpaper.nix
+++ home/hyprland/hyprpaper.nix
@@ -1,7 +1,16 @@
 {config, pkgs, self, ...}:{
 	home.file.".config/hypr/hyprpaper.conf" = {
 		text = ''
-		  preload = ${self.outPath}/assets.bg1.pnghttps://github.com/NixOS/nixos-artwork/blob/master/wallpapers/nix-wallpaper-watersplash.png?raw=true
-		''
-	}
+		  preload = ${self.outPath}/assets/bg1.png
+		  #preload = ${self.outPath}/assets/bg2.png
+		  #preload = ${self.outPath}/assets/bg3.png
+		  #preload = ${self.outPath}/assets/bg4.png
+
+			wallpaper = DP-3, ${self.outPath}/assets/bg1.png
+			wallpaper = DP-4, ${self.outPath}/assets/bg1.png
+			wallpaper = eDP-1, ${self.outPath}/assets/bg1.png
+
+			ipc = off
+		'';
+	};
 }
