2a4de671057b0490bdd8caafb5fc2b3f2170ae40
SamueleFacenda <samuele.facenda@gmail.com>
2023-09-09 18:10:06 +0200

    Fix rpl package
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{pkgs, lib, ...}: {
   home.packages = with pkgs; [
     # archives
     zip
@@ -9,6 +9,56 @@
     htop
     mdcat
     xclip
+    (rpl.overrideAttrs (finalAttrs: previousAttrs:{
+      src = fetchFromGitHub {
+        owner = "rrthomas";
+        repo = "rpl";
+        rev = "v1.15.5";
+        sha256 = "W9fbu4p2sMzD+1dMM8o45S/Q765qh7hvpYDZFZdyrtA=";
+      };
+      version = "1.15.5";
+      patches = [];
+      format = "pyproject";
+      doCheck = false;
+      postPatch = ''
+        sed -i "s/importlib.metadata.version('rpl')/'1.15.5'/g" rpl/__init__.py
+      '';
+      installPhase = "";
+
+      propagatedBuildInputs = [
+        python3.pkgs.chardet
+         python3.pkgs.regex
+                  (
+            python3.pkgs.buildPythonPackage rec {
+              pname = "argparse-manpage";
+              version = "4.4";
+              src = fetchPypi {
+                inherit pname version;
+                sha256 = "w3SucQVgZXmLnrbk2TIy3clyyc1ZZGvNnYRhkzYpD7Q=";
+              };
+              doCheck = false;
+              propagatedBuildInputs = [
+                python310Packages.toml
+              ];
+            }
+          )
+          (
+             python3.pkgs.buildPythonPackage rec {
+              pname = "chainstream";
+              version = "1.0.1";
+              src = fetchPypi {
+                inherit pname version;
+                sha256 = "302P1BixEmkODm+qTLZwaWLktrlf9cEziQ/TIVfI07c=";
+              };
+              format = "pyproject";
+              doCheck = false;
+              propagatedBuildInputs = [
+                python310Packages.setuptools
+              ];
+            }
+          )
+      ];
+    }))
 
     # misc
     xdg-utils
@@ -16,30 +66,19 @@
     wakatime
     spotify-tui
     editorconfig-core-c
-    
+
     # productivity
     obsidian
 
     # web
     brave
-    
-    # games
-    #(superTuxKart.overrideAttrs (finalAttrs: previousAttrs: {
-   #   assets = fetchsvn {
-  #        url = "https://svn.code.sf.net/p/supertuxkart/code/stk-assets";
- #         rev = "18587";
-  #        sha256 = "WNavCyf3yFtFIgTkk+IBjNY8W3Sv5+ZBiBOKTIWLPhA=";
-  #        name = "stk-assets";
-   #     };
-    #}))
 
-	# code
+    # code
     (python3.withPackages(ps: with ps; [
-    	pillow # for ranger kitty image preview
-
-    	# ranger deps
-    	chardet
-    	python-bidi
+      pillow # for ranger kitty image preview
+      # ranger deps
+      chardet
+      python-bidi
     ]))
 
     # ranger dependencies
@@ -63,7 +102,7 @@
     # openscad
     # drawio
   ];
-  
+
   programs = {
     btop.enable = true;  # replacement of htop/nmon
     exa.enable = true;   # A modern replacement for ‘ls’
@@ -76,16 +115,16 @@
     # auto mount usb drives
     udiskie.enable = true;
     udiskie.settings.program_options = {
-    	file_manager = "${pkgs.xdg-utils}/bin/xdg-open";
-    	# tray = false;
+      file_manager = "${pkgs.xdg-utils}/bin/xdg-open";
+      # tray = false;
     };
   };
-  
+
   # fix udiskie problem
   systemd.user.targets.tray = {
-		Unit = {
-			Description = "Home Manager System Tray";
-			Requires = [ "graphical-session-pre.target" ];
-		};
-	};
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = [ "graphical-session-pre.target" ];
+    };
+  };
 }
