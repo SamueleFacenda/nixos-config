b04be63a254cb57c95a5c2050476ca2c82e12ebf
SamueleFacenda <samuele.facenda@gmail.com>
2023-09-12 15:57:20 +0200

    Fix lid switch closure
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/hyprland/bindings.nix
+++ home/hyprland/bindings.nix
@@ -25,11 +25,11 @@
     # Lid switch settings
     #",switch:Lid Switch,exec,swaylock"
 
-    ",switch:on:Lid Switch,exec,hyprctl keyword monitor \"eDP-1,2736x1824,1440x1050,2\""
+    ",switch:off:Lid Switch,exec,hyprctl keyword monitor \"eDP-1,2736x1824,1440x1050,2\""
     #",switch:on:Lid Switch,exec,hyprctl keyword monitor \"DP-3,1680x1050,1440x0,1\""
     #",switch:on:Lid Switch,exec,hyprctl keyword monitor \"DP-4,1440x900,0x0,1\""
 
-    ",switch:off:Lid Switch,exec,hyprctl keyword monitor \"eDP-1, disable\""
+    ",switch:on:Lid Switch,exec,hyprctl keyword monitor \"eDP-1, disable\""
     #",switch:off:Lid Switch,exec,hyprctl keyword monitor \"DP-3, disable\""
     #",switch:off:Lid Switch,exec,hyprctl keyword monitor \"DP-4, disable\""
   ];
