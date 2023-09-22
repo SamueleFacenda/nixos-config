6566cc18f7f0e4f8d62834426d36270931c96dd9
SamueleFacenda <samuele.facenda@gmail.com>
2023-09-22 17:34:31 +0200

    Onedark theme for console
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- modules/system.nix
+++ modules/system.nix
@@ -71,7 +71,10 @@
   };
 
   # enable plymouth
-  boot.plymouth.enable = true;
+  boot.plymouth = {
+    enable = true;
+    #theme = "breeze";
+  };
 
   # enable zsh system-wide
   programs.zsh.enable = true;
@@ -171,27 +174,13 @@
   };
 
   console = {
-    packages = with pkgs; [
-      powerline-fonts
-    ];
+    packages = with pkgs; [ powerline-fonts ];
     font = "ter-powerline-v32n.psf.gz";
     colors = [ # onedark scheme
-      "282c34"
-      "353b45"
-      "3e4451"
-      "545862"
-      "565c64"
-      "abb2bf"
-      "b6bdca"
-      "c8ccd4"
-      "e06c75"
-      "d19a66"
-      "e5c07b"
-      "98c379"
-      "56b6c2"
-      "61afef"
-      "c678dd"
-      "be5046"
+      "282c34" "353b45" "3e4451" "545862"
+      "565c64" "abb2bf" "b6bdca" "c8ccd4"
+      "e06c75" "d19a66" "e5c07b" "98c379"
+      "56b6c2" "61afef" "c678dd" "be5046"
     ];
   };
 
