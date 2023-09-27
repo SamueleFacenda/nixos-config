79a1975a72192ce2fa30eceef6dfbd3e196c41ea
SamueleFacenda <samuele.facenda@gmail.com>
2023-09-27 14:50:56 +0200

    Try to fix kanshi
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/hyprland/kanshi.nix
+++ home/hyprland/kanshi.nix
@@ -1,9 +1,10 @@
 { config, pkgs, ... }:{
+  systemd.user.services.kanshi.Service.ExecReload = "${pkgs.kanshi}/bin/kanshictl reload";
   services.kanshi = {
     enable = true;
     systemdTarget = "hyprland-session.target";
     profiles = {
-      undocked.exec = "hyprctl dispatch dpms on";
+      undocked.exec = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       undocked.outputs = [
         {
           criteria = "eDP-1";
