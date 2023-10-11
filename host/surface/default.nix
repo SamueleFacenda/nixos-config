c426f72bdc97bd381173779d77adfc9fa2f1d8d2
SamueleFacenda <samuele.facenda@gmail.com>
2023-10-11 11:49:58 +0200

    Enable gstreamer for virtual camera support
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- host/surface/default.nix
+++ host/surface/default.nix
@@ -70,7 +70,19 @@
     microcodeIntel
     libwacom-surface
     libcamera-surface # custom
-    gst_all_1.gstreamer # for virtual camera support
+
+    # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
+    gst_all_1.gstreamer
+    # Common plugins like "filesrc" to combine within e.g. gst-launch
+    gst_all_1.gst-plugins-base
+    # Specialized plugins separated by quality
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    # Plugins to reuse ffmpeg to play almost every video format
+    gst_all_1.gst-libav
+    # Support the Video Audio (Hardware) Acceleration API
+    gst_all_1.gst-vaapi
   ];
 
   # add camera support to application not supporting libcamera
