f27eba2fb5ea8c88ae6f3736734deb8497c03ce6
SamueleFacenda <samuele.facenda@gmail.com>
2023-10-28 15:11:16 +0200

    Fix wake monitor position, lock screen after lid switchù
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/hyprland/swayidle.nix
+++ home/hyprland/swayidle.nix
@@ -4,7 +4,7 @@
     systemdTarget = "hyprland-session.target";
     events = [
       { event = "after-resume"; command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";}
-      # { event = "before-sleep"; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshot --effect-blur 10x7 --fade-in 5";}
+      { event = "before-sleep"; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshot --effect-blur 10x7";}
     ];
     timeouts = [
       {
