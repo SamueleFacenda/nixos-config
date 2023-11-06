c60028303e6e549381959690408d2414bf3b3f52
SamueleFacenda <samuele.facenda@gmail.com>
2023-11-06 09:14:55 +0100

    Dynamic template loading
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- templates/default.nix
+++ templates/default.nix
@@ -1,26 +1,8 @@
-{
-  cpp = {
-    path = ./cpp;
-    description = "A basic c/c++ project";
-    welcomeText = ''
-      # A simple c and c++ project
-      No bundling included, just plain cpp and gcc to run the apps.
-    '';
-  };
-  python = {
-    path = ./python;
-    description = "A basic python project";
-    welcomeText = ''
-      # A simple python 3 project
-      No bundling included, just python and some modules.
-    '';
-  };
-  default = {
-    path = ./basic;
-    description = "A basic nix flake project";
-    welcomeText = ''
-      # A simple flake
-      Some of the common options have placeholders
-    '';
-  };
-}
+builtins.mapAttrs
+    (n: v: {
+      path = ./. + "/${n}";
+      description = "My personal ${n} template";
+    })
+    (builtins.removeAttrs
+      (builtins.readDir ./.)
+      ["default.nix"])
