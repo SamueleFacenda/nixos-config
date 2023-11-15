80bbb4ceca704d644036dfce40dc2aee42273dcc
SamueleFacenda <samuele.facenda@gmail.com>
2023-11-15 11:53:54 +0100

    Fix audio, now use pipewire

--- modules/system.nix
+++ modules/system.nix
@@ -44,8 +44,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
-  # hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
