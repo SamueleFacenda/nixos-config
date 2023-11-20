1d1722e9b93968d327d601aa0cece79b2bade07b
SamueleFacenda <samuele.facenda@gmail.com>
2023-11-20 12:29:44 +0100

    Fix waybar loop script
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/scripts/waybar-loop.sh
+++ home/scripts/waybar-loop.sh
@@ -10,7 +10,8 @@ while true; do
     logger -i "$0: Starting waybar in the background..."
     waybar &
     logger -i "$0: Started waybar PID=$!. Waiting for modifications to ''${CONFIG_FILES}..."
-    inotifywait -P -e modify "$CONFIG_FILES" 2>&1 | logger -i
+    # shellcheck disable=SC2086
+    inotifywait -P -e modify $CONFIG_FILES 2>&1 | logger -i
     logger -i "$0: inotifywait returned $?. Killing all waybar processes..."
     killall .waybar-wrapped 2>&1 | logger -i
     logger -i "$0: killall .waybar-wrapped returned $?, wait a sec..."
