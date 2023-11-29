093c49c50d432d2cb3344fd4b7cc88743c76178b
SamueleFacenda <samuele.facenda@gmail.com>
2023-11-29 09:51:48 +0100

    Update wakatime to use wakapi
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/programs/wakatime.nix
+++ home/programs/wakatime.nix
@@ -7,16 +7,14 @@ if writeable
 then {
   home.activation = {
     wakatimeCfg = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if test ! -f ''${HOME}/.wakatime.cfg; then
-        $DRY_RUN_CMD printf "[settings]\napi_key = @wakatime-key@" > ''${HOME}/.wakatime.cfg
-      fi
+      $DRY_RUN_CMD printf "[settings]\napi_key = @wakatime-key@\napi_url = https://wakapi.dev/api" > ''${HOME}/.wakatime.cfg
     '';
   };
 }
 else {
   home.file.".wakatime.cfg".source = toml.generate ".wakatime.cfg" {
     settings.api_key = "@wakatime-key@";
+    settings.api_url = "https://wakapi.dev/api";
   };
-
   home.file.".wakatime.cfg".force = true;
 }
