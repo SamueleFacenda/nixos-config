8c0fff6fcc8a839cfb9689c513661b24201dd475
SamueleFacenda <samuele.facenda@gmail.com>
2023-11-29 10:18:53 +0100

    Update wakatime key management
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/programs/wakatime.nix
+++ home/programs/wakatime.nix
@@ -1,20 +1,13 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, secrets, ... }:
 let
-  writeable = true;
-  toml = pkgs.formats.toml { };
+  format = pkgs.formats.ini { };
 in
-if writeable
-then {
-  home.activation = {
-    wakatimeCfg = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      $DRY_RUN_CMD printf "[settings]\napi_key = @wakatime-key@\napi_url = https://wakapi.dev/api" > ''${HOME}/.wakatime.cfg
-    '';
-  };
-}
-else {
-  home.file.".wakatime.cfg".source = toml.generate ".wakatime.cfg" {
-    settings.api_key = "@wakatime-key@";
-    settings.api_url = "https://wakapi.dev/api";
+{
+  home.file.".wakatime.cfg".source = format.generate ".wakatime.cfg" {
+    settings = {
+      api_key_vault_cmd = "cat ${secrets.wakatime-key.path}";
+      api_url = "https://wakapi.dev/api";
+    };
   };
   home.file.".wakatime.cfg".force = true;
 }
