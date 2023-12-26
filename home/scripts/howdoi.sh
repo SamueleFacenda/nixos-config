96d9b3c1490a24695de77b79f0005a5d7d18c958
SamueleFacenda <samuele.facenda@gmail.com>
2023-12-26 21:23:53 +0100

    Create howdoi script
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

---
+++ home/scripts/howdoi.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+if [[ $# -eq 0 ]]
+then
+        echo "Usage: howdoi <question>"
+        return 1
+else
+        # gh copilot explain "How do I $@ ?"
+        gh copilot suggest -t shell "How do I $@ ?"
+        
+fi
+return 0
