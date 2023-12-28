27a8f304d7f331c5b4ca89a1184351397b7fe190
SamueleFacenda <samuele.facenda@gmail.com>
2023-12-28 14:25:48 +0100

    Write setup wizard
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/scripts/howdoi.sh
+++ home/scripts/howdoi.sh
@@ -2,11 +2,10 @@
 
 if [[ $# -eq 0 ]]
 then
-        echo "Usage: howdoi <question>"
-        return 1
+    echo "Usage: howdoi <question>"
+    return 1
 else
-        # gh copilot explain "How do I $@ ?"
-        gh copilot suggest -t shell "How do I $@ ?"
+    # gh copilot explain "How do I $@ ?"
+    gh copilot suggest -t shell "How do I $@ ?"
         
 fi
-return 0
--- packages/installer.nix
+++ packages/installer.nix
@@ -1,12 +1,83 @@
 { stdenv
 , writeShellApplication
 , git
+, gum
 }:
 
 writeShellApplication {
   name = "installer";
-  runtimeInputs = [ git ];
+  runtimeInputs = [ git gum ];
   text = ''
-    echo ciao
+    set -e
+    ORIGIN="https://github.com/SamueleFacenda/nixos-config"
+
+    function dir_permission_check() {
+      if [ -e "$1" ]; then
+        if [ -d "$1" ] && [ -w "$1" ] && [ -x "$1" ]; then
+          return 0
+        else
+          return 1
+        fi
+      else
+        dir_permission_check "$(dirname "$1")"
+        return $?
+      fi
+    }
+
+    # ask for config dir and ask for clone
+    configdir="/nixos-config"
+
+    if gum confirm "Do you want to clone the configuration?"
+    then
+      configdir=$(gum input --value "$configdir" \
+        --header="Configuration location (new directory )" )
+
+      if [ -e "$configdir" ]
+      then
+        echo "Error, path $configdir already exists"
+        exit 1
+      fi
+
+      # todo check permissions
+      if dir_permission_check "$configdir"
+      then
+        mkdir -p "$configdir"
+      else
+        echo "Not enough permission"
+        sudo mkdir -p "$configdir"
+        sudo chown "$(id -nu):$(id -ng)" "$configdir"
+        # chmod 774 "$configdir"
+      fi
+
+      gum spin --title "Cloning config in $configdir..." \
+        git clone "$ORIGIN" "$configdir"
+
+    else
+      echo "Select the configuration directory"
+      configdir="$(gum file --directory --file=false)"
+    fi
+
+    gum style \
+      --border double \
+      --align center --width 50 --margin "1 2" --padding "1 2" \
+    'Now it'"'"'s secrets time! 🗝️
+    Generate a keypary (ssh-keygen) for this machine.
+    If you are me or someone with access to a machine with a private '\
+    'key allowed to edit the secrets, add a public key from this machine to'\
+    'the keys in secrets/secrets.nix in the config.
+    Then run "agenix --rekey <filename>" on all the secrets
+    Important! You must be on a machine with already present keys
+
+    If you are not me, just remove all my public keys from secrets/secrets.nix'\
+    ', add yours, delete the secrets (*.age) and recreate them with you secrets.
+    Instructions for the secret files format are in secrets/secrets.nix'
+
+    if gum confirm "Activate the config now?"
+    then
+      hostname="$(gum input --placeholder 'config name(hostname)')"
+      sudo nixos-rebuild switch --flake "$configdir#$hostname"
+    fi
+
+    echo "Happy hacking!"
   '';
 }
