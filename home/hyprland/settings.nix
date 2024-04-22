2a0892defbc9a1e290aaed91b8e538c83949e925
SamueleFacenda <samuele.facenda@gmail.com>
2024-04-22 19:33:23 +0200

    Update monitor settings, add nvidia powermanagement
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/hyprland/settings.nix
+++ home/hyprland/settings.nix
@@ -2,17 +2,13 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      # "eDP-1,2736x1824,1440x1050,2" # builtin
-      "desc:Samsung Display Corp. 0x4190,2880x1800@60,0x900,1.875" # builtin (120hz possibles)
-      "desc:Fujitsu Siemens Computers GmbH E22W-5 YV2C027320,highres,1440x0,1" # big fujitsu
+      # "eDP-1,2736x1824,0x0,2" # builtin
+      "desc:Samsung Display Corp. 0x4190,2880x1800@60,0x0,2" # builtin (120hz possibles)
+      "desc:Fujitsu Siemens Computers GmbH E22W-5 YV2C027320,1680x1050@60,1441x-1050,1" # big fujitsu
       "desc:Ancor Communications Inc ASUS VW199 DCLMTF153087,highres,0x0,1" # small asus
-      # "desc:HSI HiTV 0x00000001,highres,0x1812,1" # projector
-      "desc:Ancor Communications Inc VX279 D5LMRS021367,highres,1441x900,1"
-      "desc:Dell Inc. DELL U2412M Y1H5T17S0N3L,highres,1536x900,1" # TODO place above the builtin
-
-      # Monitor DP-2 (ID 1):
-      # 	1920x1080@59.94000 at 1441x900
-      # 	description: Ancor Communications Inc VX279 D5LMRS021367 (DP-2 via HDMI)
+      "desc:HSI HiTV 0x00000001,highres,0x-1441,1" # projector
+      "desc:Ancor Communications Inc VX279 D5LMRS021367,1920x1080,0x-1080,1" # work
+      "desc:Dell Inc. DELL U2412M Y1H5T17S0N3L,1680x1050@60,0x-1050,1" # work dell
 
       ",preferred,auto,1" # fallback
     ];
--- host/zenbook/default.nix
+++ host/zenbook/default.nix
@@ -178,7 +178,7 @@ in {
   
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
