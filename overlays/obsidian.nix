385c734be24870dcec96e0863dedadb7e25758ba
SamueleFacenda <samuele.facenda@gmail.com>
2024-05-09 09:37:55 +0200

    Add graphviz to obsidian path
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

---
+++ overlays/obsidian.nix
@@ -0,0 +1,15 @@
+final: prev: {
+  # copy everithing but edit the desktop entry
+  # just to enable the touchpad gestures (should have done with a wrapper)
+  obsidian = prev.runCommandLocal prev.obsidian.name {} ''
+    mkdir $out
+
+    ln -st $out ${prev.obsidian}/share
+
+    mkdir $out/bin
+    source "${prev.makeWrapper}/nix-support/setup-hook" # bad way
+    makeWrapper ${prev.obsidian}/bin/obsidian $out/bin/obsidian \
+      --prefix PATH : ${prev.lib.makeBinPath [ prev.graphviz ]}
+
+  '';
+}
