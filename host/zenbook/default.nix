023c0e6739c2871171081b602825c57449a2fc2e
SamueleFacenda <samuele.facenda@gmail.com>
2024-06-09 20:58:13 +0200

    Nix update
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- host/zenbook/default.nix
+++ host/zenbook/default.nix
@@ -122,7 +122,8 @@ in
     "resume_offset=2291712"
   ];
   services.logind.lidSwitch = lib.mkForce "suspend-then-hibernate"; # hibernate only when not connected to power or monitors
-  services.logind.lidSwitchExternalPower = lib.mkForce "suspend-then-hibernate";
+  services.logind.lidSwitchExternalPower = lib.mkForce "suspend-then-hibernate";  
+  # services.logind.powerKey = lib.mkForce "suspend-then-hibernate";
 
   # Secure boot
 
