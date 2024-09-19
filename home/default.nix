f4585869c7a1330d927df1572566066df17806cb
SamueleFacenda <samuele.facenda@gmail.com>
2024-09-19 13:50:29 +0200

    Automatically garbage collect home-manager profiles
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- home/default.nix
+++ home/default.nix
@@ -5,4 +5,10 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true; # (useless if on nixos)
+  
+  nix.gc = {
+    automatic = true;
+    frequency = "weekly";
+    options = "-d";
+  };
 }
