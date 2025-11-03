bbe3f93b99f6a65bb7e0792ab7153814c6256a1e
SamueleFacenda <samuele.facenda@gmail.com>
2025-11-03 13:02:16 +0100

    Add personal and work calendars to trmnl
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- host/latitude/services.nix
+++ host/latitude/services.nix
@@ -124,8 +124,10 @@
           TRMNL_TITLE = "Calendario lezioni Samu";
           TRMNL_WEBHOOK_URL = "http://192.168.68.109:2300/api/custom_plugins/728011e6-bf28-4825-9e6d-4bcf7a59b631";
           TRMNL_ICS_URL = builtins.concatStringsSep "," [
-            "https://webapi.unitn.it/unitrentoapp/profile/me/calendar/C57C1F7D08BEF5551658CC7B2D299297A1F500FCD56280D6E03BA51A3E6E1F77"
-            "https://calendar.google.com/calendar/ical/75607d8028ce751b368530686bd514ad295458dc8c6086b7b3bcc2048b83e226%40group.calendar.google.com/public/basic.ics"
+            "https://webapi.unitn.it/unitrentoapp/profile/me/calendar/C57C1F7D08BEF5551658CC7B2D299297A1F500FCD56280D6E03BA51A3E6E1F77" # Uni
+            "https://calendar.google.com/calendar/ical/75607d8028ce751b368530686bd514ad295458dc8c6086b7b3bcc2048b83e226%40group.calendar.google.com/public/basic.ics" # TT
+            "https://calendar.google.com/calendar/ical/samuele.facenda%40gmail.com/public/basic.ics" # Personal
+            "https://calendar.google.com/calendar/ical/sfacenda%40fbk.eu/public/basic.ics" # Work
             ];
           TRMNL_DAYS = "30";
           TRMNL_TZ = "Europe/Rome";
