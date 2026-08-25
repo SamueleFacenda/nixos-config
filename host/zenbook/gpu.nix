0f7a948dca6aa6c0aa0670b9e561e91f9606ca01
SamueleFacenda <samuele.facenda@gmail.com>
2026-08-25 11:55:56 +0200

    Make second monitor work on prime offload setup (and gpu auto sleep too)
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- host/zenbook/gpu.nix
+++ host/zenbook/gpu.nix
@@ -14,8 +14,11 @@
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.latest;
     modesetting.enable = true;
-    powerManagement.enable = false; # https://forums.developer.nvidia.com/t/fixed-suspend-resume-issues-with-the-driver-version-470/187150/3
-    powerManagement.finegrained = false;
+    powerManagement = {
+      enable = true; # https://forums.developer.nvidia.com/t/fixed-suspend-resume-issues-with-the-driver-version-470/187150/3
+      finegrained = true;
+      kernelSuspendNotifier = true;
+    };
     open = true;
     nvidiaSettings = true;
     dynamicBoost.enable = true; # nvidia-powerd, should make changes only on AC
@@ -30,16 +33,18 @@
         enableOffloadCmd = true;
       };
     };
+    moduleParams.nvidia = {
+      NVreg_DynamicPowerManagementVideoMemoryThreshold = 1024;
+      NVreg_TemporaryFilePath = "/var/tmp";
+    };
   };
 
   boot.kernelParams = lib.mkAfter [
-    "nvidia.NVreg_DynamicPowerManagementVideoMemoryThreshold=1024"
     # "nvidia.NVreg_S0ixPowerManagementVideoMemoryThreshold=1024"
     # "nvidia.NVreg_EnableS0ixPowerManagement=1"
-    # "nvidia.NVreg_TemporaryFilePath=/var/tmp"
     # "nvidia.NVreg_EnableGpuFirmware=0"
     # "nvidia.NVreg_UseKernelSuspendNotifiers=1"
-    "nvidia.NVreg_DynamicPowerManagement=0x03"
+    # "nvidia.NVreg_DynamicPowerManagement=0x03" finegrained
     # "nvidia.NVreg_UsePageAttributeTable=1"
     # "nvidia.NVreg_InitializeSystemMemoryAllocations=0"
     # "nvidia-drm.fbdev=0"
@@ -58,7 +63,7 @@
     ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
   '';
 
-  # Change prime mode and some hyprland env vars (hdmi doesn't work with offload)
+  # Change prime mode and some hyprland env vars (hdmi doesn't work with offload). Update, it does on normal config as long as I don't suspend
   specialisation.multi-monitor.configuration = {
     home-manager.users.samu.wayland.windowManager.hyprland.settings.env = [
       # for hyprland with nvidia gpu, ref https://wiki.hyprland.org/Nvidia/
@@ -74,25 +79,16 @@
       # prime.sync.enable = lib.mkForce true;
       powerManagement.finegrained = lib.mkForce false;
     };
-    environment.sessionVariables = let name = config.users.default.name; in {
-      AQ_DRM_DEVICES = lib.mkForce "/home/${name}/.config/hypr/intel:/home/${name}/.config/hypr/nvidia";
-      GSK_RENDERER = lib.mkForce null;
-      __EGL_VENDOR_LIBRARY_FILENAMES = lib.mkForce null;
-    };
+    environment.sessionVariables.GSK_RENDERER = lib.mkForce null;
   };
 
-  environment.systemPackages = with pkgs; [
-    nvtopPackages.nvidia
-  ];
+  environment.systemPackages = [ pkgs.nvtopPackages.nvidia ];
 
-  # Use these or the standard nvidia settings (not working now)
-  services.supergfxd.enable = false;
-
-  environment.sessionVariables = {
-    AQ_DRM_DEVICES = "/home/${config.users.default.name}/.config/hypr/intel"; # no nvidia, it keeps an open fd on the card anyway
-    __EGL_VENDOR_LIBRARY_FILENAMES = "${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json";
+  environment.sessionVariables = let name = config.users.default.name; in {
+    AQ_DRM_DEVICES = "/home/${name}/.config/hypr/intel:/home/${name}/.config/hypr/nvidia";
     # Don't use vulkan on GTK, avoid GPU wake
     GSK_RENDERER =  "ngl";
   };
   home-manager.users.samu.systemd.user.services.swaync.Service.Environment = [ "GSK_RENDERER=ngl" ];
+  home-manager.users.samu.systemd.user.services.swayosd.Service.Environment = [ "GSK_RENDERER=ngl" ];
 }
